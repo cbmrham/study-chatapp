26f8b088e5be3d672a75d3c1729776d66f0a2bcf
cbmrham <chibimarucham@gmail.com>
2023-07-18 18:20:49 +0000

    commit

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,13 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
+import { UserService } from '@study-chatapp/data-access-users';
 
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly user: UserService
+  ) {}
 
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @Get('users')
+  async getUsers() {
+    const users = this.user.users({});
+
+    return users;
   }
 }
